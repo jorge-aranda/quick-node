37bb3a9d55d9ebfa75111e118f3b2be8cb714c74
jorge-aranda <jorgearanda123@gmail.com>
2016-02-27 17:00:01 +0100

    Code clean

--- src/main/java/es/jaranda/quick/node/api/controller/GreetingController.java
+++ src/main/java/es/jaranda/quick/node/api/controller/GreetingController.java
@@ -3,17 +3,11 @@ package es.jaranda.quick.node.api.controller;
 
 import static es.jaranda.quick.node.api.constants.UrlMappingConstants.*;
 import es.jaranda.quick.node.batch.job.GreetingJobContext;
-import java.util.Date;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.job.builder.SimpleJobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -24,14 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// XXX In a future, this controller will be disappear. To launch job you will do POST to /job {'jobName':'greet'}
+// XXX In a future, this controller will be disappear. To launch job you will do POST to /job {'jobName':'greet', 'jobParameters':{}}
 
 @Controller
 @RequestMapping(GREETING_URL_MAPPING)
 public class GreetingController {
-    
-    private static final Logger LOG = 
-            LoggerFactory.getLogger(GreetingController.class);
 
     @Autowired
     private JobLauncher jobLauncher;
